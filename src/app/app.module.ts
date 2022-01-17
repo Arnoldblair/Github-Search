8b28da3a84632b07b0a397ea5c6b0bd34fa09aeb
Arnoldblair <arnoldblair.munene@student.moringaschool.com>
2022-01-17 20:41:45 +0300

    add pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,34 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DisplayProfileComponent } from './display-profile/display-profile.component';
-import { DisplayRepositoriesComponent } from './display-repositories/display-repositories.component';
+import { FormsModule } from '@angular/forms';
 import { FormComponent } from './form/form.component';
+import { RepositoriesComponent } from './repositories/repositories.component';
+import { DisplayRepositoriesComponent } from './display-repositories/display-repositories.component';
+import { DatePassedPipe } from './date-passed.pipe';
+
+import { HighlightDirective } from './highlight.directive';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { RepositoriesComponent } from './repositories/repositories.component';
-import { UserServiceComponent } from './user-service/user-service.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DisplayProfileComponent,
-    DisplayRepositoriesComponent,
-    FormComponent,
     HomeComponent,
-    NavbarComponent,
+    DisplayProfileComponent,
+    FormComponent,
     RepositoriesComponent,
-    UserServiceComponent
+    DisplayRepositoriesComponent,
+    NavbarComponent,
+    DatePassedPipe,
+    HighlightDirective,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
